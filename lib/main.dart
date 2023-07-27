6dac7f15ba93a60ea5739a07acf64b955b871e5b
kauan.meira <kauangabriel.meira123@gmail.com>
2023-07-27 16:04:04 -0300

    comunicação com API giphy e implementação de layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,23 @@
+import 'package:buscador_gifs/pages/home_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
-}
-
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+  runApp(MaterialApp(
+    home: const HomePage(),
+    theme: ThemeData(
+      primaryColor: Colors.white,
+      hintColor: Colors.white,
+      inputDecorationTheme: InputDecorationTheme(
+        enabledBorder: OutlineInputBorder(
+          borderSide: const BorderSide(color: Colors.white),
+          borderRadius: BorderRadius.circular(8.0),
         ),
+        focusedBorder: OutlineInputBorder(
+          borderSide: const BorderSide(color: Colors.white),
+          borderRadius: BorderRadius.circular(8.0),
+        ),
+        labelStyle: const TextStyle(color: Colors.white),
       ),
-    );
-  }
+    ),
+  ));
 }
